7558acdaf44d41b698291facad0979a592376ce4
devitridwan <devitridwan@gmail.com>
2021-10-06 19:18:01 +0700

    Delete rebuild.sh

--- rebuild.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sudo docker stop test_sagara
-sudo docker rm test_sagara
-sudo docker rmi test_sagara
-sudo docker build -t test_sagara .
-sudo docker run -tid -p 8800:8800 --network="host" --name test_sagara test_sagara ./MainEndPoint
